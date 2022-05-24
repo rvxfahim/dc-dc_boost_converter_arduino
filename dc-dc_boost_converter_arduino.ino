9aa1a93492693ddc12131a0770e92d7f3978fe5b
Tahmid Zahin <rvxfahim@gmail.com>
2022-05-24 16:58:49 +0600

    using error amplifier

--- dc-dc_boost_converter_arduino.ino
+++ dc-dc_boost_converter_arduino.ino
@@ -4,34 +4,78 @@
  */
  
 #include "TimerOne.h"
+#include <digitalWriteFast.h>
  int vOut;
- int dutycycle=1023;
+ int dutycycle=1000;
+ bool dir=false;
 void setup()
 {
-  Serial.begin(115200);
-  Timer1.initialize(10);         // initialize timer1, and set a 1/2 second period
-  Timer1.pwm(9, dutycycle);                // setup pwm on pin 9, 50% duty cycle
+  //Serial.begin(500000);
+  Timer1.initialize(20);         // initialize timer1
+  Timer1.pwm(9, dutycycle);                // setup pwm on pin 9, duty cycle
   pinMode(13, OUTPUT);
+  pinMode(2, OUTPUT);
   digitalWrite(13, HIGH);
+  //pinModeFast(7, INPUT);
+  pinMode(7,INPUT);
+  //attachInterrupt(digitalPinToInterrupt(7), decrease, CHANGE);
 }
 
  
 void loop()
 {
-  vOut = analogRead(A0);
-  float vOut1 = vOut * (5.0 / 1023.0);
-  float current=vOut1/1000.0;
-  float v1=current*10000;
-  float total=v1+vOut1;
-  if (total>12)
-  {
-    dutycycle++;
+//  vOut = analogRead(A0);
+//  float vOut1 = vOut * (5.0 / 1023.0);
+//  float current=vOut1/2631;
+//  float v1=current*10000;
+//  float total=v1+vOut1;
+//  if (total>12)
+//  {
+//    dutycycle--;
+//    Timer1.setPwmDuty(9, dutycycle);
+//    }
+//    else
+//    {
+//      dutycycle++;
+//    Timer1.setPwmDuty(9, dutycycle);
+//      }
+//  if (total<12.3 && total>11.8)
+//  {
+//    digitalWrite(2, HIGH);
+//    }
+//  else
+//  digitalWrite(2,LOW);
+//  //Serial.println(total);
+//  //float iOut=analogRead(A1);
+//  //Serial.println("Current: " + String(iOut));
+
+//  if(digitalReadFast(7)==HIGH)
+//    {
+//      dutycycle--;
+//      Timer1.setPwmDuty(9, dutycycle);
+//      //Serial.println("decreasing duty");
+//      }
+// else
+//    {
+//      dutycycle++;
+//      Timer1.setPwmDuty(9, dutycycle);
+//      //Serial.println("inreasing duty");
+//      }
+
+if(digitalRead(7)){
+    
+     dutycycle--;
+     if(dutycycle<5)
+     dutycycle=5;
     Timer1.setPwmDuty(9, dutycycle);
     }
-    else
-    {
-      dutycycle--;
+    else{
+     
+      dutycycle++;
+      if(dutycycle>1015)
+      dutycycle=1015;
     Timer1.setPwmDuty(9, dutycycle);
-      }
-  //Serial.println(total);
+    
+   }
+
 }
